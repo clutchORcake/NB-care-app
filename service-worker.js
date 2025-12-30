49618ce99f842ec54e65403556c56f6842435f70
WJ <ccwin7270@gmail.com>
2025-12-30 22:50:55 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,28 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "baby-cheatsheet-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/nav.html",
+  "/cheatsheet.html",
+  "/safety.html",
+  "/signals.html",
+  "/tips.html",
+  "/warning.html",
+  "/style.css",
+  "/js/script.js",
+  "/images/denied.png"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('newborn-care-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './styles.css'
-      ]);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
