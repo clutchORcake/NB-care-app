987eb8880718dd7475895722f5317019fdab1b52
WJ <ccwin7270@gmail.com>
2025-12-30 23:28:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ function checkPassword() {
     window.location.href = "nav.html";
   } else {
     document.getElementById('error-msg').innerHTML = `
-      <p style="color:#ff69b4; font-weight:bold;">Access Denied 🚫</p>
-      <img src="images/denied.png" alt="Access Denied! cuz /"Jasmine is not mine/" xD" style="width:200px; margin-top:10px;">
+      <p style="color:#ff69b4; font-weight:bold;">Access Denied 🚫 cuz /"Jasmine is not mine/" xD</p>
+      <img src="images/denied.png" alt="Access Denied" style="width:200px; margin-top:10px;">
     `;
   }
 }
@@ -42,4 +42,5 @@ if('serviceWorker' in navigator){
 //     .then(() => console.log('Service Worker Registered'))
 //     .catch(err => console.log('SW registration failed:', err));
 // }
+
 
